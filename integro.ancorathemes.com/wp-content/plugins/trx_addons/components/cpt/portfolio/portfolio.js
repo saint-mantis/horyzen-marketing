fcc29b5a4b64234d9363bbc064ff1e97abd06911
Arun Babu <59362586+saint-mantis@users.noreply.github.com>
2024-07-07 01:04:58 +0530

    updates

---
+++ integro.ancorathemes.com/wp-content/plugins/trx_addons/components/cpt/portfolio/portfolio.js
@@ -0,0 +1,23 @@
+jQuery(document).on('action.init_hidden_elements', function(e, cont) {
+    "use strict";
+    cont.find('.sc_portfolio_masonry_wrap,[class*="portfolio_page_gallery_type_masonry_"]').each(function() {
+        var portfolio = jQuery(this);
+        if (portfolio.parents('div:hidden,article:hidden').length > 0) return;
+        if (!portfolio.hasClass('inited')) {
+            portfolio.addClass('inited');
+            trx_addons_when_images_loaded(portfolio, function() {
+                setTimeout(function() {
+                    portfolio.masonry({
+                        itemSelector: portfolio.hasClass('sc_portfolio_masonry_wrap') ? '.sc_portfolio_masonry_item' : '.portfolio_page_gallery_item',
+                        columnWidth: portfolio.hasClass('sc_portfolio_masonry_wrap') ? '.sc_portfolio_masonry_item' : '.portfolio_page_gallery_item',
+                        percentPosition: true
+                    });
+                    setTimeout(function() {
+                        jQuery(window).trigger('resize');
+                        jQuery(window).trigger('scroll');
+                    }, 100);
+                }, 0);
+            });
+        } else {}
+    });
+});
